1a1f6cf519d5f2096e22193bd0cffd0ae3128b47
Brandon22Adams <41342071+Brandon22Adams@users.noreply.github.com>
2025-11-23 15:00:45 -0700

    Dialog setup

--- src/app/pages/projects/project-dialog-wrapper/project-dialog-wrapper.ts
+++ src/app/pages/projects/project-dialog-wrapper/project-dialog-wrapper.ts
@@ -1,17 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ProjectID } from '../projects.const';
+import { MatIconModule } from '@angular/material/icon';
+import { SintasClayworks } from '../sintas-clayworks/sintas-clayworks';
+import { Ghostie } from '../ghostie/ghostie';
+import { BreakOut } from '../break-out/break-out';
+import { Froge } from '../froge/froge';
+import { GlitchWar } from '../glitch-war/glitch-war';
+import { ToolBelt } from '../tool-belt/tool-belt';
 
 @Component({
   selector: 'app-project-dialog-wrapper',
-  imports: [],
+  imports: [MatIconModule, SintasClayworks, Ghostie, BreakOut, Froge, GlitchWar, ToolBelt],
   templateUrl: './project-dialog-wrapper.html',
   styleUrl: './project-dialog-wrapper.scss',
 })
 export class ProjectDialogWrapper {
   readonly dialogRef = inject(MatDialogRef<ProjectDialogWrapper>);
-  readonly data = inject<{id: ProjectID}>(MAT_DIALOG_DATA);
+  public readonly data = inject<{id: ProjectID}>(MAT_DIALOG_DATA);
+  public readonly projectIDs = ProjectID;
 
+  constructor() {}
 
+  public close(): void {
+    this.dialogRef.close();
+  }
 
 }
--- src/app/pages/projects/projects.const.ts
+++ src/app/pages/projects/projects.const.ts
@@ -10,6 +10,10 @@ export interface IProject {
 export enum ProjectID {
   sintas,
   ghostie,
+  breakOut,
+  froge,
+  glitchWar,
+  toolBelt
 }
 
 export const PROJECTS_CONST: Array<IProject> = [
@@ -28,5 +32,37 @@ export const PROJECTS_CONST: Array<IProject> = [
     imgSrc: 'ghostie.png',
     imgAlt: 'Ghostie game',
     description: 'Ranked 15th out of 87 entries in a game jam. Designed and built entirely in Unity and C#.',
-  }
+  },
+  {
+    id: ProjectID.toolBelt,
+    title: 'Stardew ToolBelt Mod',
+    icon: 'build',
+    imgSrc: 'placeholder.svg',
+    imgAlt: 'ToolBelt Mod',
+    description: 'Created c# mod for StardewValley with more than 6,770 unique downloads',
+  },
+    {
+    id: ProjectID.froge,
+    title: 'Froge',
+    icon: 'gamepad',
+    imgSrc: 'placeholder.svg',
+    imgAlt: 'Froge',
+    description: 'Ranked #28th for enjoyment out of 287 entries in a game jam. Built in Unity and C# in 3 days',
+  },
+    {
+    id: ProjectID.glitchWar,
+    title: 'Glitch War',
+    icon: 'gamepad',
+    imgSrc: 'placeholder.svg',
+    imgAlt: 'Glitch war',
+    description: 'Built in Unity and c# released on Steam. Successfully funded on Kickstarter.',
+  },
+    {
+    id: ProjectID.breakOut,
+    title: 'Break Out',
+    icon: 'gamepad',
+    imgSrc: 'placeholder.svg',
+    imgAlt: 'Breakout',
+    description: 'C# and Unity learning projects that I build for my dad.',
+  },
 ]
